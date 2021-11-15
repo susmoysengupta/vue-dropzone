5bfcda055497173600743c5089836d434b298e12
SusmoySenGupta <susmoy.cse@gmail.com>
2021-11-15 14:24:10 +0600

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: 'jit',
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: 'class', // or 'media' or 'class'
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {},
     },
