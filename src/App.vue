7797e79b5715d556ad0c6de95ef73c71f673ea10
SusmoySenGupta <susmoy.cse@gmail.com>
2021-11-15 13:34:29 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-gray-50 w-full min-h-screen flex items-center justify-center dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center">
+            <img alt="Vue logo" src="./assets/logo.png" class="w-32"/>
+            <p class="text-xl font-semibold text-gray-800 dark:text-gray-100">Vue 3 Drop Zone</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
